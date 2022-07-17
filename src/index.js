e35bf135ee570686e40029ca38a5f9a54727b0fd
Kolenkaa <kkolenkaa@gmail.com>
2022-07-17 22:29:37 +0300

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	do {
+		str = str.replace("()", "");
+		str = str.replace("{}", "");
+		str = str.replace("[]", "");
+		str = str.replace("||", "");
+		str = str.replace("12", "");
+		str = str.replace("34", "");
+		str = str.replace("56", "");
+		str = str.replace("77", "");
+		str = str.replace("88", "");
+	} while (
+		str.includes("()") ||
+		str.includes("{}") ||
+		str.includes("[]") ||
+		str.includes("||") ||
+		str.includes("12") ||
+		str.includes("34") ||
+		str.includes("56") ||
+		str.includes("77") ||
+		str.includes("88")
+	);
+	const result = (e) => (e > 0 ? false : true);
+	return result(str.length);
 }
